46fced68efd3c93c00ac3a183d1cf5d74e86b948
Richard <richwats@cisco.com>
2022-07-15 11:41:16 +1000

    vars

--- variables.tf
+++ variables.tf
@@ -1,1 +1,12 @@
+### ACI Credentials & Users - from TFC Variable Set ###
+variable "aci_user" {
+}
+
+variable "aci_password" {
+}
+
+variable "aci_url" {
+}
+
+### Placeholder ###
 variable "fabric" {}
